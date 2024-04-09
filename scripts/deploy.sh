2f425112147d33331121a7120efc7a858b40466e
Richard Maxwell <richard@rsmaxwell.co.uk>
2024-04-09 21:52:06 +0100

    add debug

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,6 +8,23 @@ PIPELINE_DIST_DIR=${PIPELINE_DIR}/dist
 
 . ${PIPELINE_PACKAGE_DIR}/info
 
+
+
+if [ -f ${HOME}/.m2/maven-repository-info ]; then
+    . ${HOME}/.m2/maven-repository-info
+elif [ -f ./maven-repository-info ]; then
+    . ./maven-repository-info
+fi
+
+if [ -z "${MAVEN_REPOSITORY_BASE_URL}" ]; then
+    echo "'MAVEN_REPOSITORY_BASE_URL' is not defined"
+    exit 1
+fi
+
+REPOSITORY_URL="${MAVEN_REPOSITORY_BASE_URL}/${REPOSITORY}"
+
+
+
 mvn --batch-mode --errors deploy:deploy-file \
 	-DgroupId=${GROUPID} \
 	-DartifactId=${ARTIFACTID} \
--- scripts/package.sh
+++ scripts/package.sh
@@ -73,23 +73,6 @@ ZIPFILE=${ARTIFACTID}_${VERSION}.${PACKAGING}
 
 
 
-if [ -f ${HOME}/.m2/maven-repository-info ]; then
-    . ${HOME}/.m2/maven-repository-info
-elif [ -f ./maven-repository-info ]; then
-    . ./maven-repository-info
-fi
-
-if [ -z "${MAVEN_REPOSITORY_BASE_URL}" ]; then
-    echo "'MAVEN_REPOSITORY_BASE_URL' is not defined"
-    exit 1
-fi
-
-MAVEN_REPOSITORY_URL="${MAVEN_REPOSITORY_BASE_URL}/${REPOSITORY}"
-
-
-
-
-
 
 
 cat <<EOT >> info
@@ -107,10 +90,8 @@ PROJECT="${PROJECT}"
 GROUPID="${GROUPID}"
 FAMILY="${FAMILY}"
 ARCHITECTURE="${ARCHITECTURE}"
-BASE_URL="${BASE_URL}"
 REPOSITORY="${REPOSITORY}"
 REPOSITORYID="${REPOSITORYID}"
-REPOSITORY_URL="${REPOSITORY_URL}"
 EOT
 
 
